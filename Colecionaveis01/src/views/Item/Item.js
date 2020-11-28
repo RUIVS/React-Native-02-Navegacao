59956b2a481ac426db16153b579ac854b63291ee
RUIVS <aoshinx64@gmail.com>
2020-11-28 15:58:40 +0000

    Rotas e Navegação (02)
    Signed-off-by: RUIVS <aoshinx64@gmail.com>

---
+++ Colecionaveis01/src/views/Item/Item.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import estiloItem from './estiloItem';
+
+function Item({ navigation }) {
+
+    const voltar = () => {
+        navigation.navigate('Inicial')
+    }
+    
+    return(
+        <View style={estiloItem.container}>
+            <View style={estiloItem.borda}>
+
+                <Text style={estiloItem.texto}>Item</Text>
+
+                <TouchableOpacity style={estiloItem.botaoContainer} onPress={voltar}>
+                    <Text style={estiloItem.botaoTexto}>Voltar</Text>
+                </TouchableOpacity>
+
+            </View>
+        </View>
+    )
+}
+export default Item;
---
+++ Colecionaveis01/src/views/Perfil/estiloPerfil.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+const estiloPerfil = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    borda: {
+        backgroundColor: '#081a31',
+        height: '28%',
+        width: '80%',
+        borderRadius: 10
+    },
+    texto: {
+        fontSize: 20,
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: 10
+    },
+    botaoContainer: {
+        marginTop: 15,
+        marginBottom: 20,
+        marginLeft: 35,
+        marginRight: 35,
+        borderRadius: 5,
+        height: 40,
+        borderWidth: 2,
+        borderColor: '#e8f2e9',
+        backgroundColor: '#14417b',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    botaoTexto: {
+        fontSize: 18,
+        color: '#FFFFFF',
+        fontWeight: 'bold'
+    }
+});
+export default estiloPerfil;
